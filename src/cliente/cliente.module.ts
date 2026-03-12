cf1f11df661957d376100fd442753b6e5329bd3f
renanferreiraslima-hub <renanferreiraslima@gmail.com>
2026-03-12 18:36:57 -0300

    final entidade cliente

--- src/cliente/cliente.module.ts
+++ src/cliente/cliente.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cliente } from './entities/cliente.entity';
-import { ClienteController } from './cliente.controller';
-import { ClienteService } from './cliente.service';
+import { ClienteController } from './controllers/cliente.controller';
+import { ClienteService } from './services/cliente.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Cliente])],
--- src/cliente/entities/cliente.entity.ts
+++ src/cliente/entities/cliente.entity.ts
@@ -1,21 +1,32 @@
-import { Entity, PrimaryColumn, Column } from 'typeorm';
+import { Entity, PrimaryColumn, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+import { Transform, TransformFnParams } from 'class-transformer';
+
 
 @Entity({ name: 'tb_clientes' })
 export class Cliente {
 
-  @PrimaryColumn({ type: 'varchar', length: 255 })
-  id: string;
+  @PrimaryGeneratedColumn ()
+  id: number;
 
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsNotEmpty()
   @Column({ type: 'varchar', length: 255, nullable: false })
   nome: string;
 
-
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsNotEmpty()
+  @IsEmail()
   @Column({ type: 'varchar', length: 255, nullable: false })
   email: string;
 
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @MinLength(8)
+  @IsNotEmpty()
   @Column({ type: 'varchar', length: 50, nullable: false })
   senha: string;
 
+  @IsNotEmpty()
   @Column({ type: 'date', nullable: false })
   dataNascimento: Date;
 
@@ -25,10 +36,14 @@ export class Cliente {
     default: 'https://ik.imagekit.io/leu4crxxao/games/istockphoto-2041572395-612x612.jpg'
   })
   foto: string;
-
+  
+  @Transform(({ value }: TransformFnParams) => value?.trim())  
+  @IsNotEmpty()
   @Column({ type: 'varchar', length: 11, nullable: false })
   cpf: string;
 
+  @Transform(({ value }: TransformFnParams) => value?.trim())  
+  @IsNotEmpty()
   @Column({ type: 'varchar', length: 11, nullable: false })
   telefone: string;
 
