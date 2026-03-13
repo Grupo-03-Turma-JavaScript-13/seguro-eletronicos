a66a29c30de1313ae18b4b3d982a2412205afa66
Livia Machado <liviamcampos98@gmail.com>
2026-03-12 23:01:03 -0300

    🐛 FIX:  Correção do projeto base

--- src/apolice/apolice.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApoliceService } from './apolice.service';
-import { ApoliceController } from './apolice.controller';
-
-@Module({
-  controllers: [ApoliceController],
-  providers: [ApoliceService],
-})
-export class ApoliceModule {}
